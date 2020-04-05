7b6f9c7e0b54dc07028e60d0a094dae233d02984
Amit-Milo <56229281+Amit-Milo@users.noreply.github.com>
2020-04-05 20:08:09 +0300

    Add files via upload
    initial commit

---
+++ ViewModels/IDashboardViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightSimulatorApp.ViewModels
+{
+    interface IDashboardViewModel
+    {
+        float headingDeg { get; set; }
+        float verticalSpeed { get; set; }
+        float groundSpeed { get; set; }
+        float airSpeed { get; set; }
+        float gpsAltitude { get; set; }
+        float internalRoll { get; set; }
+        float internalPitch { get; set; }
+        float altimerAltitude { get; set; }
+    }
+}
